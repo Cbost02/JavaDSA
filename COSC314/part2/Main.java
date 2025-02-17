511608cb402900ef15d20fb690dac213190f3847
Cromwell Bost <cbost@emich.edu>
2025-02-17 16:37:43 -0500

    support files added

---
+++ COSC314/part2/Main.java
@@ -0,0 +1,9 @@
+package COSC314.part2;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ COSC314/part3/Main.java
@@ -0,0 +1,9 @@
+package COSC314.part3;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
